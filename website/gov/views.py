c32e26c427f9457572bbf9da2e74813d4ae18210
Elwyne Mabanglo <elwyne.mabanglo@gmail.com>
2018-02-25 00:38:44 +0000

    Working version of csv upload

--- website/gov/views.py
+++ website/gov/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy, reverse
 from .models import Data
 import logging
 import sys
+import csv
 
 class IndexView(generic.ListView):
     template_name = 'gov/index.html'
@@ -23,6 +24,7 @@ class DataCreate(CreateView):
 
 def upload_csv(request):
     data = {}
+
     if "GET" == request.method:
         return render(request, "gov/upload_csv.html", data)
     # if not GET, then proceed
@@ -34,37 +36,35 @@ def upload_csv(request):
     #if file is too large, return
     if csv_file.multiple_chunks():
         return HttpResponseRedirect(reverse("gov:upload_csv"))
-
+    
     file_data = csv_file.read().decode("utf-8")     
 
     lines = file_data.split("\r\n")
-
-
+    
     for line in lines[1:]:        
 
         fields = line.split(",")
         
         if (len(fields) > 1):
 
-            try:
-                m = Data(property_name = fields[0],
-                            property_address1 = 'not empty' if (fields[1]) else '',
-                            property_address2 = '',#fields[2].strip(),
-                            property_address3 = '',#fields[3].strip(),
-                            property_address4 = '',#fields[4].strip(),
-                            unit_name = '',#fields[5].strip(),
-                            tenant_name = '',#fields[6].strip(),
-                            lease_start_date = "2012-01-02",
-                            lease_end_date = "2012-01-02",
-                            lease_years = "0",
-                            current_rent = "0.0"
-                            )
-                #m.save()
-
+            #try:
+            m = Data(
+                property_name = fields[0],
+                property_address1 = fields[1],
+                property_address2 = fields[2],
+                property_address3 = fields[3],
+                property_address4 = fields[4],
+                unit_name = fields[5],
+                tenant_name = fields[6],
+                lease_start_date = "2012-01-02", #fields[7],
+                lease_end_date = "2012-01-02", #fields[8],
+                lease_years = fields[9],
+                current_rent = fields[10]
+            )
          
-                m.save()
-            except:
-                err = sys.exc_info()[0]
-                return HttpResponse(f'line: {line} ; fields: {fields}, error: {err}')
+            m.save()
+            #except:
+            #    err = sys.exc_info()[0]
+            #    return HttpResponse(f'line: {line} ; fields: {fields}, error: {err}')
 
     return HttpResponseRedirect(reverse("gov:index"))
